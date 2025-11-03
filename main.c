8b8c0f4f02caa082a8eec4f1ec0872142c9684a3
ayesha-afaq <bla4753ayesha@gmail.com>
2025-11-03 22:52:56 +0500

    main func

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-void get_user_input(char*text, int size);
+void user_input(char*text, int size);
+int get_option();
+
 
 int main(){
 
@@ -10,16 +12,79 @@ int main(){
     // array for text
     char text[MAX_LENGTH];
 
-    get_user_input(text, MAX_LENGTH);
+    // user_input(text, MAX_LENGTH);
 
-    // printf("you wrote this: \n");
-    // printf(text);
+    int run=1;
+    while(run){
+        int option_num= get_option();
+
+        switch (option_num)
+        {
+        case 1:
+            // func call
+            break;
+        case 3:
+            break;
+            // func call
+        case 4:
+            break;
+            // func call
+        case 5:
+            break;
+            // func call
+        case 6:
+            break;
+            // func call
+        case 7:
+            break;
+            // func call
+        case 8:
+            break;
+            // func call
+        case 9:
+            printf("Exititng Program");
+            run=0;
+            break;
+
+        default:
+            printf("Invalid Option");
+        }
+
+    }
     
     return 0;
  
 }
 
-void get_user_input(char*text,int size){
+int get_option(){
+    int option_num;
+    int result;
+
+    printf("\n1.first\n2.second\n3.third\n4.fourth\n5.fifth\n6.sixth\n7.seven\n8.eigth\n9.exit");
+
+    while(1){
+        printf("\nChoose Option: ");
+        result= scanf("%d",&option_num);
+
+        if (result != 1){
+            // scanf failed (user didn't enter a number)
+            printf("Invalid input! Please enter a number between 1 and 9.\n");
+
+            // clear leftover characters from input buffer
+            while (getchar() != '\n');
+                continue; // ask again
+        }
+
+        if (option_num>=1 && option_num<=9)
+            break;
+        else{printf("Invalid! Please enter between 1 and 9");}
+
+    }
+    return option_num;
+
+}
+
+void user_input(char*text,int size){
     char line[512];
     // initialize to an empty string
     text[0]='\0';  
