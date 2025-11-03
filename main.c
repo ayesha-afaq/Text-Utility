6c7a4d590f41ef10c8d13bbcad51336d5d701125
Areeba Javed <areeba.javaid2005@gmail.com>
2025-11-03 22:26:29 +0500

    uppercase + analyzer

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 void get_user_input(char*text, int size);
 
@@ -43,4 +44,47 @@ void get_user_input(char*text,int size){
     }
 
 
+}
+void convert_to_uppercase(char* text) {
+
+
+    //to use the func call convert_to_uppercase(text); for ayesha 
+    for (int i = 0; text[i] != '\0'; i++) {
+        text[i] = toupper((unsigned char) text[i]);
+    }
+    printf("\nConverted Text in Capital Letters:\n");
+    printf("%s\n", text);
+}
+
+void analyze_text(const char* text) {
+    // to use the func call analyze_text(text); for ayesha
+    int word_count = 0;
+    int sentence_count = 0;
+    int char_count = 0;
+    int in_word = 0;
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        char ch = text[i];
+
+        if (!isspace((unsigned char)ch)) {
+            char_count++;
+        }
+
+        if (ch == '.' || ch == '?' || ch == '!') {
+            sentence_count++;
+        }
+
+        if (!isspace((unsigned char)ch)) {
+            if (!in_word) {
+                word_count++;
+                in_word = 1;
+            }
+        } else {
+            in_word = 0;
+        }
+    }
+
+    printf("\nWord Count: %d", word_count);
+    printf("\nSentence Count: %d", sentence_count);
+    printf("\nCharacter Count (non-space): %d\n", char_count);
 }
